e0c35af0477715814b2f3ddc730cbfc1aa14040f
Gianni Rivera <giannirivera@192.168.1.50>
2026-07-20 23:44:11 -0500

    Configurar PostgreSQL y conexión con SQLAlchemy

---
+++ backend/app/test_connection.py
@@ -0,0 +1,17 @@
+from sqlalchemy import text
+
+from backend.app.database.database import engine
+
+
+try:
+    with engine.connect() as connection:
+        result = connection.execute(text("SELECT version();"))
+
+        print("=================================")
+        print("Conexión exitosa a PostgreSQL")
+        print(result.scalar())
+        print("=================================")
+
+except Exception as error:
+    print("Error al conectar con PostgreSQL:")
+    print(error)
